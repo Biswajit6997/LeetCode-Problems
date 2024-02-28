de85a5bd528c03253e056d48275ee73b8890f0da
Biswajit6997 <biswajitde1996@gmail.com>
2024-02-29 00:09:07 +0530

    first commit

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,50 @@
+public class RemoveDuplicatesFromSortedArray {
+
+    public static void main(String[] args) {
+        int [] num={1,1,2};
+        System.out.println(removeDuplicates(num));
+        
+    }
+
+public  static int removeDuplicates(int[] nums){
+    int count=0;
+    for(int i=0;i<nums.length;i++){
+        if(i<nums.length-1 && nums[i]==nums[i+1]){
+            continue;
+        }
+        else{
+            nums[count]=nums[i];
+            count++;
+        }
+    }
+    return count;
+
+}
+// public static void main(String[] args)
+// {
+//     int nums[]={0,3,3,4,4,5,5};
+//     System.out.println(removeDuplicates(nums));
+
+// }
+
+
+// public static int removeDuplicates(int[] nums)
+// {
+//    int count=0;
+//    for(int i=0;i<nums.length;i++)
+//    {
+//        if(i<nums.length-1 && nums[i]==nums[i+1])
+//        {
+//            continue;
+//        }
+
+//        else
+//        {
+//            nums[count]=nums[i];
+//            count++;
+//        }
+//    }
+
+//    return count;
+// }
+}
