a59699e9b67ef80e2889b6b04ca673a29b602910
Biswajit6997 <biswajitde1996@gmail.com>
2024-06-24 19:50:42 +0530

    add Leetcode POTD 955 Minimum Number of k Consecutive Bit Flips

---
+++ 24-06-2024.java
@@ -0,0 +1,30 @@
+//Approach-1 (Using auxiliary array to mark Flipped indices)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int minKBitFlips(int[] nums, int k) {
+        int n = nums.length;
+
+        int flips = 0;
+        boolean[] isFlipped = new boolean[n];
+        int flipCountFromPastForCurri = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i >= k && isFlipped[i - k]) {
+                flipCountFromPastForCurri--;
+            }
+
+            if (flipCountFromPastForCurri % 2 == nums[i]) {
+                if (i + k > n) {
+                    return -1;
+                }
+                flipCountFromPastForCurri++;
+                isFlipped[i] = true;
+                flips++;
+            }
+        }
+
+        return flips;
+    }
+}
+
