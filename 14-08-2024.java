a3fa35db26c130efe6c0c94d2c6b53fc00583671
Biswajit6997 <biswajitde1996@gmail.com>
2024-08-14 21:38:10 +0530

    Add Leetcode POTD 719. Find K-th Smallest Pair Distance

---
+++ 14-08-2024.java
@@ -0,0 +1,26 @@
+//Approach-1 (storing the distances and finding the kth smallest)
+//T.C : O(n^2)
+//S.C : O(maxEl)
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        int n = nums.length;
+        int maxEl = Arrays.stream(nums).max().getAsInt();
+        int[] vec = new int[maxEl + 1];
+
+        // All pairs
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int d = Math.abs(nums[i] - nums[j]);
+                vec[d]++;
+            }
+        }
+
+        for (int d = 0; d <= maxEl; d++) {
+            k -= vec[d];
+            if (k <= 0) {
+                return d; // Returning kth smallest distance
+            }
+        }
+        return -1;
+    }
+}
