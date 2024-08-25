64b6c28ba66b2f4e776d56966cd8bab0fe0c20e8
Biswajit6997 <biswajitde1996@gmail.com>
2024-08-25 22:47:32 +0530

    Add Leetcode POTD 145. Binary Tree Postorder Traversal

---
+++ 25-08-2024.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        postorderHelper(root, result);
+        return result;
+    }
+    
+    private void postorderHelper(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        
+        // Traverse left subtree
+        postorderHelper(node.left, result);
+        // Traverse right subtree
+        postorderHelper(node.right, result);
+        // Visit the root
+        result.add(node.val);
+    }
+}
