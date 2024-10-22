3523b0fda4f0caa0ae33db390424f2c03d9f2193
Biswajit6997 <biswajitde1996@gmail.com>
2024-10-22 22:38:21 +0530

    Add Prac Prob on Pointer

---
+++ 22-10-2024.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    // int a=10;
+    // int *iptr=&a;
+    // printf("%d",sizeof(iptr));
+
+
+    int *i,*j;
+    float *f;
+    double *d;
+    printf("%d\n",i);
+    printf("%d\n",j);
+    printf("%d\n",i+4);
+    printf("%d\n",i-3);
+    printf("%d\n",i++);
+    printf("%d\n",++i);
+    printf("%d\n",i--);
+    printf("%d\n",--i);
+    printf("%d\n",i-j);
+    // printf("%d\n",i+j);
+
+}
