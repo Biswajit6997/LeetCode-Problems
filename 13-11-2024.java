4577ee958754ef628a034010b3a3e53c20f4a6eb
Biswajit6997 <biswajitde1996@gmail.com>
2024-11-13 23:00:58 +0530

    Add leetcode POTD

---
+++ 13-11-2024.java
@@ -0,0 +1,26 @@
+/ Java equivalent of lower_bound in C++
+    private int lowerBound(int[] nums, int start, int end, int target) {
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+    // Java equivalent of upper_bound in C++
+    private int upperBound(int[] nums, int start, int end, int target) {
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
