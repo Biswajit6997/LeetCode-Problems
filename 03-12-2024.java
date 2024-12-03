deb84ac9e35ec0506407c5e1837d66be0087f061
Biswajit6997 <biswajitde1996@gmail.com>
2024-12-03 21:33:50 +0530

    Add GFG POTD Problem

---
+++ 03-12-2024.java
@@ -0,0 +1,22 @@
+//Approach (Simple traversal using two pointers)
+//T.C : O(m+n)
+//S.C : O(1)
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+        int m = s.length();
+        int n = spaces.length;
+
+        StringBuilder result = new StringBuilder(); // Using StringBuilder for efficiency
+        int j = 0; // Pointer to traverse the spaces array
+
+        for (int i = 0; i < m; i++) {
+            if (j < n && i == spaces[j]) {
+                result.append(" ");
+                j++;
+            }
+            result.append(s.charAt(i));
+        }
+
+        return result.toString();
+    }
+}
