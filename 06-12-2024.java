24c1c44d270e435e8d341b9fb890b5e70277a5c4
Biswajit6997 <biswajitde1996@gmail.com>
2024-12-06 23:19:45 +0530

    Add LeetCode POTD Problem

---
+++ 06-12-2024.java
@@ -0,0 +1,30 @@
+/************************************************************ JAVA *********************************************************/
+//Approach - Simple Greedily select smallest numbers
+//T.C : O(n)
+//S.C : O(m)
+class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : banned) {
+            set.add(num);
+        }
+
+        int count = 0;
+        int sum = 0;
+
+        for (int num = 1; num <= n; num++) {
+            if (set.contains(num)) {
+                continue;
+            }
+
+            if (sum + num <= maxSum) {
+                count++;
+                sum += num;
+            } else {
+                break;
+            }
+        }
+
+        return count;
+    }
+}
