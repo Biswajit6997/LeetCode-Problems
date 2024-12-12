cbb9647f6b2c2b6235225e93e1ce72cbc0fee76e
Biswajit6997 <biswajitde1996@gmail.com>
2024-12-12 23:26:04 +0530

    Add LeetCode POTD Problem

---
+++ 12-12-2024.java
@@ -0,0 +1,30 @@
+//Approach (Using max-heap)
+//T.C : O(n + k*logn)
+//S.C : O(n)
+public class Solution {
+
+    public long pickGifts(int[] gifts, int k) {
+        
+        List<Integer> giftsList = new ArrayList<>();
+        for (int gift : gifts) {
+            giftsList.add(-gift);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(giftsList);
+        while(k-- > 0) {
+            
+            int maxElement = -pq.poll();
+
+            
+            pq.offer(-(int) Math.sqrt(maxElement));
+        }
+
+        // Accumulate the sum of the elements in the heap
+        long numberOfRemainingGifts = 0;
+        while (!pq.isEmpty()) {
+            numberOfRemainingGifts -= pq.poll(); //since it's negative, it will become positive with -ve sign
+        }
+
+        return numberOfRemainingGifts;
+    }
+}
