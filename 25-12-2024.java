c5928b795f778a2ba7e464be9764a36a7813552c
Biswajit6997 <biswajitde1996@gmail.com>
2024-12-25 22:59:44 +0530

    ADD Leetcode  POTD

---
+++ 25-12-2024.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<Integer> result = new ArrayList<>();
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                max = Math.max(max, current.val);
+                
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+            result.add(max);
+        }
+        return result;
+    }
+}
