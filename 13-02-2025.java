3345d6455bc1ce9fe11122b3d0f43e2c24ff058b
Biswajit6997 <biswajitde1996@gmail.com>
2025-02-13 22:46:37 +0530

    Add LeetCode POTD

---
+++ 13-02-2025.java
@@ -0,0 +1,26 @@
+//Approach (Using Heap)
+//T.C : Onlogn)
+//S.C : O(n)
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        
+        // Add all elements to the priority queue
+        for (int num : nums) {
+            pq.add((long) num);
+        }
+
+        int count = 0;
+
+        while (!pq.isEmpty() && pq.peek() < k) {
+            long smallest = pq.poll();
+            
+            long secondSmallest = pq.poll(); 
+
+            pq.add(smallest * 2 + secondSmallest); // Push the new value back to the heap
+            count++;
+        }
+
+        return count;
+    }
+}
