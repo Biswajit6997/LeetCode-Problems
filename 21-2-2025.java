e02b694e6cee3aae289993b0b792685fa7244728
Biswajit6997 <biswajitde1996@gmail.com>
2025-02-21 10:12:46 +0530

    Add LeetCode POTD

---
+++ 21-2-2025.java
@@ -0,0 +1,25 @@
+// Approach-1 (Using DFS)
+// T.C : O(n)
+// S.C : O(n)
+class FindElements {
+    Set<Integer> st = new HashSet<>();
+
+    private void dfs(TreeNode root, int x) {
+        if (root == null)
+            return;
+
+        root.val = x;
+        st.add(x);
+        dfs(root.left, 2 * x + 1);
+        dfs(root.right, 2 * x + 2);
+    }
+
+    public FindElements(TreeNode root) {
+        st.clear();
+        dfs(root, 0);
+    }
+
+    public boolean find(int target) {
+        return st.contains(target);
+    }
+}
