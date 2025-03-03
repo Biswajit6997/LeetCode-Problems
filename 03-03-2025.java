5be3c76e53fc71aa53a6f2762b74dac51265cfb8
Biswajit6997 <biswajitde1996@gmail.com>
2025-03-03 22:22:50 +0530

    Add LeetCode POTD

---
+++ 03-03-2025.java
@@ -0,0 +1,38 @@
+//Approach-2 (Using pointers only)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+
+        int countLess = 0;
+        int countEqual = 0;
+
+        for (int num : nums) {
+            if (num < pivot)
+                countLess++;
+            else if (num == pivot)
+                countEqual++;
+        }
+
+        int i = 0; // less than pivot
+        int j = countLess; // equal to pivot
+        int k = countLess + countEqual; // more than pivot
+        int[] result = new int[n];
+
+        for (int num : nums) {
+            if (num < pivot) {
+                result[i] = num;
+                i++;
+            } else if (num == pivot) {
+                result[j] = num;
+                j++;
+            } else {
+                result[k] = num;
+                k++;
+            }
+        }
+
+        return result;
+    }
+}
