262c2ada51a71a1e564ed8507d30dacc514590a9
Biswajit6997 <biswajitde1996@gmail.com>
2025-04-08 22:44:33 +0530

    ADD LeetCode POTD

---
+++ 08-04-2025.java
@@ -0,0 +1,19 @@
+
+//Approach-2 (Using reverse loop)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int n = nums.length;
+        Set<Integer> st = new HashSet<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (st.contains(nums[i])) {
+                return (int)Math.ceil((i + 1) / 3.0);
+            }
+            st.add(nums[i]);
+        }
+
+        return 0;
+    }
+}
