df5c90f47d9d4136d5c152eb88e81349333a9149
Biswajit6997 <biswajitde1996@gmail.com>
2025-04-27 22:54:56 +0530

    ADD LeetCode POTD

---
+++ 27-04-2025.java
@@ -0,0 +1,17 @@
+//Approach (Striaght forward simulation)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int countSubarrays(int[] nums) {
+        int n = nums.length;
+        int result = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] == (nums[i - 1] + nums[i + 1])*2) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
