97b253df5665dab7f515a7f051083fea2494626c
BISWAJIT DEY <113124621+Biswajit6997@users.noreply.github.com>
2025-07-22 20:53:09 +0530

    Add Leetcode POTD

---
+++ 22-07-2025.java
@@ -0,0 +1,26 @@
+//Approach-1 (Sliding Window)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        
+        int i = 0; 
+        int j = 0;
+        int sum = 0;
+        int max = 0;
+        
+        while(j < nums.length) {
+            if(!set.contains(nums[j])) {
+                sum += nums[j];
+                max = Math.max(max, sum); 
+                set.add(nums[j++]);
+            } else {
+                sum -= nums[i];
+                set.remove(nums[i++]);
+            }
+        }
+        
+        return max;
+    }
+}
